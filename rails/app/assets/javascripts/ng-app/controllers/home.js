003a4d873fe3496e21778cc94940511fd39c72d5
libowen <libowen@gmail.com>
2016-07-13 14:34:37 -0700

    fix home ctrl

--- rails/app/assets/javascripts/ng-app/controllers/home.js
+++ rails/app/assets/javascripts/ng-app/controllers/home.js
@@ -1,4 +1,4 @@
 angular.module('mainApp')
-.controller('HomeCtrl', function ($scope) {
+.controller('HomeCtrl', ["$scope", function ($scope) {
     $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
-});
+}]);
